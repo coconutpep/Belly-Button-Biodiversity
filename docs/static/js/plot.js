ceb9e148e492409d1846ca4530ab71234e503b9e
unknown <ryanklueg@gmail.com>
2020-05-09 13:19:01 -0400

    added patient select

--- docs/static/js/plot.js
+++ docs/static/js/plot.js
@@ -1,3 +1,8 @@
-const samples = d3.json("data/samples.json").then(function(data) {
+//Set up selector to create dashboard controller
+const select = d3.select("select");
 
+//Read is json data
+const samples = d3.json("data/samples.json").then(function(data) {
+    names = data.names;
+    names.forEach(name => select.append("option").text(name));
 });
